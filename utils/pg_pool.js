6321693a56903df2927dbb204027ceee5fc82b75
ZManak <javifuer@gmail.com>
2023-01-28 18:41:07 +0100

    established DB connections

--- utils/pg_pool.js
+++ utils/pg_pool.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
-// const queries = require('./queries')
-// const {DB_HOST, DB_USER, DB_DATABASE, DB_PASSWORD} = process.env
+const {DB_HOST, DB_USER, DB_DATABASE, DB_PASSWORD} = process.env
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: "trumpet.db.elephantsql.com", 
-    user: "qebgidaw",
-    database: "qebgidaw",
-    password: "5DtcQ_d7y5eMW84qIZW8whyHgSA6TGk6"
-  })  
+    host: DB_HOST, 
+    user: DB_USER,
+    database: DB_DATABASE,
+    password: DB_PASSWORD
+  })
+
   
   
   pool.connect((err, client, release) => {
