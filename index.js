fe8d6740a4034328249f9b7b9c8a8a8a0a777236
ZManak <javifuer@gmail.com>
2023-01-28 04:14:07 +0100

    added/login

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const request = require('request')
+const morgan = require('morgan')
+
+
+
+const app = express()
+const port = 3000
+
+// Template engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Middlewares
+app.use(express.json()); // Habilitar tipo de dato a recibir
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+app.post('/', function (req, res) {
+    let email = req.body.email;
+    console.log(email)
+    res.render("signup")
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+app.listen(port, () => {
+    console.log("server running")
+})
