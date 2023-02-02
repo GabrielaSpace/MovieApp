10267777ca6a006d91b5b39e59ad31e4a4cdbc72
ZManak <javifuer@gmail.com>
2023-02-02 13:33:03 +0100

    fix/routes, styles

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const homeRoutes = require('./routes/homeRoutes')
 const dashboardRoutes = require('./routes/dashboardRoutes')
 const searchRoutes = require('./routes/searchRoutes')
 const createMovie = require('./routes/createMovieRoutes')
+const favmovies = require('./routes/userRoutes')
 
 const app = express();
 const port = 3000;
@@ -37,6 +38,7 @@ app.use('/', homeRoutes)
 app.use('/dashboard', dashboardRoutes)
 app.use('/search', searchRoutes)
 app.use('/createmovie',createMovie)
+app.use('/favmovies', favmovies)
 
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ userRouter.post('/', userCrontrolers.createUser);
 /* entriesApiRouter.delete('/', entriesApiController.deleteEntry);
 entriesApiRouter.put('/', entriesApiController.updateEntry); */
 userRouter.post('/search/:title', userCrontrolers.addFavorite)
-userRouter.get('/favmovies', userCrontrolers.getFavorites)
+userRouter.get('/', userCrontrolers.getFavorites)
 
 module.exports = userRouter
 
