b7de458270355d3a55aa5ea391351522fef6e7e3
PabMatMar <pabmatmar@gmail.com>
2023-02-02 14:12:18 +0100

    add/ refactor

--- public/moviesAdminScripts.js
+++ public/moviesAdminScripts.js
@@ -4,12 +4,14 @@ import { deleteFavorite } from './utils/data.js';
 
 
 
-// for (let i = 0; i < buttons.length; i++) {
+const buttons = document.getElementsByClassName("delete")
+console.log(buttons)
+for (let i = 0; i < buttons.length; i++) {
 
-    document.getElementById(`delete0`).addEventListener('click', async (e) => {
+    document.getElementById(`delete${i}`).addEventListener('click', async (e) => {
         e.preventDefault;
-        let movie = document.getElementById(`title0`).innerHTML;
-        console.log("este es valor de h3:"+movie)
+        let movie = document.getElementById(`title${i}`).innerHTML;
+        console.log("Este es valor de h3:"+movie)
         const cleanTitle = movie.slice(7,)
         const titleMovie = cleanTitle.trim()
         console.log(titleMovie)
@@ -19,4 +21,4 @@ import { deleteFavorite } from './utils/data.js';
             console.log(deleteResponse)
         }
     })
-// }
+}
