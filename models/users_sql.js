b529adde2818723e0d279e6bc17f5d4f41acb3a2
PabMatMar <pabmatmar@gmail.com>
2023-02-05 21:57:10 +0100

    add / restructured scraping functions, adapted and connected to the search paths by title

--- models/users_sql.js
+++ models/users_sql.js
@@ -35,40 +35,6 @@ const validatedUser = async (user) => {
     return result
 } 
 
-// function login(user, callback) {
-//     //this example uses the "pg" library
-//     //more info here: https://github.com/brianc/node-postgres
-
-//     const { email } = user
-
-//     const bcrypt = require('bcrypt');
-//     const pool = require('../utils/pg_pool');
-
-//     pool.connect((err, client, done) => {
-//         if (err) return callback(err);
-
-//         const query = 'SELECT user_id, email, password FROM users WHERE email = $1';
-//         client.query(query, [email], (err, result) => {
-//             // NOTE: always call `done()` here to close
-//             // the connection to the database
-//             done();
-
-//             if (err || result.rows.length === 0) return callback(err || new WrongUsernameOrPasswordError(email));
-
-//             const user = result.rows[0];
-
-//             bcrypt.compare(password, user.password, function (err, isValid) {
-//                 if (err || !isValid) return callback(err || new WrongUsernameOrPasswordError(email));
-
-//                 return callback(null, {
-//                     user_id: user.id,
-//                     email: user.email
-//                 });
-//             });
-//         });
-//     });
-// }
-
 const addFavorite = async (fav) => {
     const { user, title, year, director, genre, runtime, img } = fav;
     let client, result;
