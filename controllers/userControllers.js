3f79ca5e3e3c4f0cfce69c2a018f0d320a8996c3
PabMatMar <pabmatmar@gmail.com>
2023-02-07 15:22:43 +0100

    add/ call to getfavorites through the id of the logged in user working

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,9 +12,10 @@ const users = require('../models/users_sql')
 const getFavorites = async (req, res) => {
     let userData = req.oidc.user
     let userId = userData.sub
+    const padding = "        "
     // console.log("Este es el console.log de userData:"+userData)
     console.log("Este es el console.log de userId:"+userId)
-    const userMovies = await users.getFavorites(userId);
+    const userMovies = await users.getFavorites(userId+padding);
     res.status(200).render("moviesUser", { favMovies: userMovies });
 };
 
