888ddb56e7d97e73c598b047e73a39599e9f5599
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-02-07 13:13:24 +0100

    jsdoc without auth

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -1,3 +1,9 @@
+/**
+ * @author Javier Fuertes, Gabriela García y Pablo Mateos 
+ * @exports search
+ * @namespace searchControllers
+ */
+
 require('dotenv').config();
 const Movies = require('../models/moviesMongo');
 const scraper = require('../utils/scraper');
@@ -8,18 +14,38 @@ const getSearch = (req, res) => {
     res.render('search');
 }
 
+
+/**
+ * Description: This function gets all the movies in the database.
+ * @memberof searchControllers
+ * @method startScraping
+ * @async 
+ * @param {string} title - The title to search for.
+ * @return {Object} - an object containing the scraped info.
+ * @throws {Error} message with the error during the scraping process.
+ */
+
+
 const startScraping = async (title) => {
     try {
-        // ---Descomenta las 2 siguientes líneas para hacer scraping---
         const products = await scraper.scrap("https://www.filmaffinity.com/en/search.php?stype=title&stext=" + title);
-        // console.log(products)
-        return products /*.json({ Critics: products[0].Critics, Puntuacion: products[0].Punctuation });*/
-        // res.status(200).json({"mensaje":"Aquí irán los productos"}); // ---Comenta esta línea---
-    } catch (error) {
+        return products 
+    } catch (error) {                                                                             
         console.log("Error Scraping")
     }
 
 }
+/**
+ * Description: This function gets all the movies in the database.
+ * @memberof searchControllers
+ * @method getSearchForTitleInMongo 
+ * @async 
+ * @param {Object} req HTTP request object
+ * @param {Object} res HTTP response object
+ * @param {string} title - The title to search for.
+ * @return {Object} - an object containing the scraped info.
+ * @throws {Error} message with the error during the search process.
+ */
 
 
 const getSearchForTitleInMongo = async (req, res) => {
@@ -29,16 +55,25 @@ const getSearchForTitleInMongo = async (req, res) => {
     console.log(param)
     if (param !== undefined) {
         const critics = await startScraping(title)
-        console.log("ENTRE EN SEARCH SEARCH MONGO")
-        console.log(critics)
         res.status(200).render("searchInMongoForTitle", { param, critics: critics })
-
     } else {
         const title = "/search/" + req.params.title
         res.redirect(title)
     }
 
 }
+
+/**
+ * Description: This function gets all the movies in the database.
+ * @memberof searchControllers
+ * @method getSearchForTitle
+ * @async 
+ * @param {Object} req HTTP request object
+ * @param {Object} res HTTP response object
+ * @param {string} title - The title to search for.
+ * @return {Object} - an object containing the scraped info.
+ * @throws {Error} message with the error during the search process.
+ */
 
 const getSearchForTitle = async (req, res) => {
 
@@ -58,6 +93,18 @@ const getSearchForTitle = async (req, res) => {
     }
 }
 
+
+/**
+ * Description: This function gets all the movies in the database.
+ * @memberof searchControllers
+ * @method postFilmForm
+ * @async 
+ * @param {Object} req HTTP request object
+ * @param {Object} res HTTP response object
+ * @param {string} title - The title to search for.
+ * @return {void} The function does not return any value.
+
+ */
 const postFilmForm = async (req, res) => {
 
     const title = "/search/local/" + req.body.title.toLowerCase()
