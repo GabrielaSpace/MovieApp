657c27af74082814b287367a1319a2d6b9e13dd6
ZManak <javifuer@gmail.com>
2023-02-09 11:22:34 +0100

    fix scraping

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,3 +1,4 @@
+const chrome = require('chrome-aws-lambda');
 const puppeteer = require("puppeteer-core");
 
 const extractSensacineData = async (url, browser) => {
@@ -15,10 +16,16 @@ const extractSensacineData = async (url, browser) => {
     }
 }
 const scrap = async (url) => {
+    const options = {
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
     try {
         const scrapedData = []
-        // console.log("Opening the browser......");
-        const browser = await puppeteer.launch({ headless: true })
+        const browser = await puppeteer.launch(options)
         const page = await browser.newPage();
         await page.goto(url);
         // console.log(`Navigating to ${url}...`);
@@ -27,8 +34,8 @@ const scrap = async (url) => {
         // console.log("url capuradas", urls)
         // const urls2 = urls.slice(0, 1);
         // console.log(`${urls.length} links encontrados`);
-        for (filmaffinityLink in urls) {
-            const filmaffinity = await extractSensacineData(urls[filmaffinityLink], browser)
+        for (i in urls) {
+            const filmaffinity = await extractSensacineData(urls[i], browser)
             if(filmaffinity.Title) {
             scrapedData.push(filmaffinity)
             }
