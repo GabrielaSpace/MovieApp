6701b70351fdbbe0977adb98ec3edaf2cf4d658e
ZManak <javifuer@gmail.com>
2023-02-19 12:26:31 +0100

    add/adminRoutes

--- controllers/dashboardControllers.js
+++ controllers/dashboardControllers.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const app = express();
-const { auth } = require('express-openid-connect');
-const { SECRET, BASE_URL, CLIENT_ID, ISSUER } = process.env
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: SECRET,
-    baseURL: BASE_URL,
-    clientID: CLIENT_ID,
-    issuerBaseURL: ISSUER
-};
-
-app.use(auth(config))
 /**
  * Description: This function renders the dashboard view
  * @memberof Renders
